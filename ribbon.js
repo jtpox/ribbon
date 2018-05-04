a1883a732251bf071bc62c06a82a0aa3d1d064d2
jtPox <me@jtpox.com>
2018-05-04 13:40:11 +0800

    Initial commit for gh-pages

--- ribbon.js
+++
@@ -1,9 +0,0 @@
-/*
- * snake_case for variables and file names.
- * camelCase for functions, classes, constants.
- */
-const Express = require('express');
-const Log = require('./app/console');
-const Ribbon = require('./app/ribbon');
-
-const ribbon = new Ribbon(Express, Express(), new Log());
