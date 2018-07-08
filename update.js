cf193d9f82816bbc4d4b9b5156df823fa3e8961a
jtPox <me@jtpox.com>
2018-07-08 21:42:19 +0800

    Updated upgrade.js

--- update.js
+++ update.js
@@ -1,8 +1,32 @@
 import DotEnv from 'dotenv';
 
+import Prompt from 'prompt';
+
 DotEnv.config();
 
-console.log('\x1b[47m\x1b[35m', 'ribbon Update', '\x1b[0m');
-console.log('\x1b[42m\x1b[30m', 'Update successful.', '\x1b[0m');
-console.log('\x1b[42m\x1b[30m', 'Restart ribbon to implement changes.', '\x1b[0m');
-process.exit();
+Prompt.start();
+const Schema = {
+  properties: {
+    version: {
+      description: 'Upgrade from',
+      required: true,
+      default: '2018.7.3',
+    },
+  },
+};
+
+Prompt.get(Schema, async (prompt_err, prompt) => {
+
+  switch (prompt.version) {
+    case '2018.7.3':
+      break;
+
+    default:
+      break;
+  }
+
+  console.log('\x1b[47m\x1b[35m', 'ribbon Update', '\x1b[0m');
+  console.log('\x1b[42m\x1b[30m', 'Update successful.', '\x1b[0m');
+  console.log('\x1b[42m\x1b[30m', 'Restart ribbon to implement changes.', '\x1b[0m');
+  process.exit();
+});
