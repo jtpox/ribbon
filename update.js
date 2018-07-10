376409bffb3b4b83d8b1c731f7a1bec60b9313ac
jtPox <me@jtpox.com>
2018-07-10 21:36:39 +0800

    Updated package.json

--- update.js
+++ update.js
@@ -10,22 +10,23 @@ const Schema = {
     version: {
       description: 'Upgrade from',
       required: true,
-      default: '2018.7.3',
+      default: '2018.6.30',
     },
   },
 };
 
+console.log('\x1b[47m\x1b[35m', 'ribbon Update', '\x1b[0m');
+
 Prompt.get(Schema, async (prompt_err, prompt) => {
 
   switch (prompt.version) {
-    case '2018.7.3':
+    case '2018.6.30':
       break;
 
     default:
       break;
   }
 
-  console.log('\x1b[47m\x1b[35m', 'ribbon Update', '\x1b[0m');
   console.log('\x1b[42m\x1b[30m', 'Update successful.', '\x1b[0m');
   console.log('\x1b[42m\x1b[30m', 'Restart ribbon to implement changes.', '\x1b[0m');
   process.exit();
