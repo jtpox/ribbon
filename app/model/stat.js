6f3eb3199b9c8a516be021e98aaa9a2d283e77d5
jtPox <me@jtpox.com>
2018-09-02 17:35:10 +0800

    Added option in env to use stats.

--- app/model/stat.js
+++ app/model/stat.js
@@ -32,57 +32,59 @@ let Stat = Db.model('Stat', schema);
  */
 Stat.post = (id, populate = false) => {
   const fields = ['post', 'address', 'created_at', 'last_updated'];
-  const query = (populate)? Stat.find({ post: id }).select(fields.join(' '))
-  .populate('post') : Stat.find({ post: id }).select(fields.join(' ')); 
-  
+  const query = (populate) ? Stat.find({ post: id }).select(fields.join(' '))
+    .populate('post') : Stat.find({ post: id }).select(fields.join(' '));
+
   return query;
   // return query.exec(cb);
 };
 
 Stat.page = (id, populate = false) => {
   const fields = ['page', 'address', 'created_at', 'last_updated'];
-  const query = (populate)? Stat.find({ page: id }).select(fields.join(' '))
-  .populate('page') : Stat.find({ page: id }).select(fields.join(' '));
+  const query = (populate) ? Stat.find({ page: id }).select(fields.join(' '))
+    .populate('page') : Stat.find({ page: id }).select(fields.join(' '));
 
   return query;
 };
 
 Stat.record = (type = 'post', id, ip, useragent) => {
-  /*
-   * Check if IP has access the page or post more than 24 hours ago.
-   */
-  const cutOff = new Date();
-  cutOff.setDate(cutOff.getDate() - 2); // Current day minus 2.
+  if (process.env.RECORD_STAT.toLowerCase() == 'true') { // This way because dotenv stores as strings instead of boolean.
+    /*
+     * Check if IP has access the page or post more than 24 hours ago.
+     */
+    const cutOff = new Date();
+    cutOff.setDate(cutOff.getDate() - 2); // Current day minus 2.
 
-  const find = (type == 'post')? {
-    post: id,
-    address: ip,
-    created_at: {
-      '$lt': new Date(),
-      '$gte': cutOff,
-    }
-  } : {
-    page: id,
-    address: ip,
-    created_at: {
-      '$lt': new Date(),
-      '$gte': cutOff,
-    }
-  };
-
-  Stat.find(find, (err, docs) => {
-    if (docs.length < 1) {
-      const newRecord = new Stat({
-        post: (type == 'post')? id : null,
-        page: (type == 'page')? id : null,
+    const find = (type == 'post') ? {
+      post: id,
+      address: ip,
+      created_at: {
+        '$lt': new Date(),
+        '$gte': cutOff,
+      }
+    } : {
+        page: id,
         address: ip,
-        platform: useragent.platform,
-        browser: useragent.browser,
-        os: useragent.os,
-      });
-      newRecord.save();
-    }
-  });
+        created_at: {
+          '$lt': new Date(),
+          '$gte': cutOff,
+        }
+      };
+
+    Stat.find(find, (err, docs) => {
+      if (docs.length < 1) {
+        const newRecord = new Stat({
+          post: (type == 'post') ? id : null,
+          page: (type == 'page') ? id : null,
+          address: ip,
+          platform: useragent.platform,
+          browser: useragent.browser,
+          os: useragent.os,
+        });
+        newRecord.save();
+      }
+    });
+  }
 };
 
 export default Stat;
