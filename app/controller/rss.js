05e91a1c6e87d0a6fbbbb04ffbc4131ae0b8b702
jtPox <me@jtpox.com>
2019-05-10 00:07:08 +0800

    Moved RSS syntax to a separate library.

--- app/controller/rss.js
+++ app/controller/rss.js
@@ -1,3 +1,5 @@
+import RssGen from '../rss';
+
 import Post from '../model/post';
 
 class Rss {
@@ -9,36 +11,25 @@ class Rss {
        * Constructing the feed.
        */
 
-      let posts = '';
+      const feed = new RssGen(`${process.env.SITE_DOMAIN}/rss`);
       let lastUpdate = null;
 
       latest.forEach((post, index) => {
         lastUpdate = (lastUpdate == null) ? post.createdAt : lastUpdate;
-        posts += `<item>
-        <title>${post.title}</title>
-        <description>${post.no_tags_short}</description>
-        <link>${process.env.SITE_DOMAIN}/post/${post.url}</link>
-        <guid>${process.env.SITE_DOMAIN}/post/${post.url}</guid>
-    </item>`;
+        feed.item({
+          title: post.title,
+          description: post.no_tags_short,
+          link: `${process.env.SITE_DOMAIN}/post/${post.url}`,
+          guid: `${process.env.SITE_DOMAIN}/post/${post.url}`,
+        });
       });
       // res.json(latest);
-
-      const feed = `<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-  <channel>
-    <title>${process.env.SITE_NAME}</title>
-    <description>${process.env.SITE_NAME}</description>
-    <link>${process.env.SITE_DOMAIN}</link>
-    <atom:link href="${process.env.SITE_DOMAIN}/rss" rel="self" type="application/rss+xml" />
-    ${posts}
-  </channel>
-</rss>`;
 
       /*
        * Setting header and replying the request.
        */
       res.setHeader('Content-Type', 'application/rss+xml');
-      res.send(feed);
+      res.send(feed.output());
     } catch (err) {
       req.log.error(err);
     }
---
+++ app/rss.js
@@ -0,0 +1,39 @@
+class Rss {
+  /*
+   * url = URL of the RSS feed.
+   */
+  constructor(url) {
+    this.url = url;
+    this.items = '';
+  }
+
+  /*
+   * Add item to feed.
+   * Example: item = { title:xxx, description:xxx, link:xxx, guid:xxx }
+   */
+  item(item) {
+    let feed = '<item>';
+    Object.keys(item).forEach((key) => {
+      feed += `<${key}>${item[key]}</${key}>`;
+    });
+    feed += '</item>';
+
+    // Add feed into items list.
+    this.items += feed;
+  }
+
+  output() {
+    return `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>${process.env.SITE_NAME}</title>
+    <description>${process.env.SITE_NAME}</description>
+    <link>${process.env.SITE_DOMAIN}</link>
+    <atom:link href="${this.url}" rel="self" type="application/rss+xml" />
+    ${this.items}
+  </channel>
+</rss>`;
+  }
+}
+
+export default Rss;
